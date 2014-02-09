c4ff2dfc62a4563448069dbb472d41a8a27514d4
Luca Gherardi <lucaghera8@gmail.com>
2014-02-09 23:36:42 +0100

    Changes in example

--- TestCDM/contextMonitor.cpp
+++ TestCDM/contextMonitor.cpp
@@ -1,19 +1,29 @@
 
 #include "ros/ros.h"
 #include "std_msgs/String.h"
+#include "std_msgs/Int32.h"
+
 // string_cdm publisher and subscriber
 ros::Publisher string_cdm_pub;
 ros::Subscriber string_cdm_sub;
 
+// integer_cdm publisher and subscriber
+ros::Publisher integer_cdm_pub;
+ros::Subscriber integer_cdm_sub;
 
 
-void string_cdm_callback(const std_msgs::String::ConstPtr& msg){
+void string_cdm_callback(const std_msgs::String::ConstPtr& input){
 
 	std_msgs::String output;
-		
-	output.data = msg->data;
-	string_cdm_pub.publish(output);
-	
+	output = input;
+	string_cdm_pub.publish(output);	
+}
+
+void integer_cdm_callback(const std_msgs::Int32::ConstPtr& input){
+
+	std_msgs::Int32 output;
+	getLenght(input, output);
+	integer_cdm_pub.publish(output);	
 }
 
 int main(int argc, char** argv){
@@ -24,6 +34,8 @@ int main(int argc, char** argv){
 	
 	string_cdm_pub = nodeHandle.advertise<std_msgs::String>("string_cdm",1000);
 	string_cdm_sub = nodeHandle.subscribe("chatter", 1000, string_cdm_callback);
+	integer_cdm_pub = nodeHandle.advertise<std_msgs::Int32>("integer_cdm",1000);
+	integer_cdm_sub = nodeHandle.subscribe("chatter", 1000, integer_cdm_callback);
 	
 	ros::spin();
 	
